66c45c481bd5f7be0720a0879655b60c29fd606d
jacksongs <jacksongs@gmail.com>
2014-05-06 15:09:16 +1000

    Updated GA and DISQUS

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,3 +35,6 @@ DEFAULT_PAGINATION = 7
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+GOOGLE_ANALYTICS = 'UA-33090301-1'
+DISQUS_SITENAME = 'jacksongs'
