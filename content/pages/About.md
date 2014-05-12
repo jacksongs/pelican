81c8c1efc37cdcee6b1df86ea56507a31dc922c8
Jackson Gothe-Snape <jacksongs@gmail.com>
2014-05-12 22:43:29 +1000

    New commit

---
+++ content/pages/About.md
@@ -0,0 +1,7 @@
+Title: About Jackson
+
+I'm a data journalist working for the Adelaide Advertiser based in Canberra. 
+
+That means I do a [bit of federal politics](http://www.adelaidenow.com.au/news/south-australia/treasurer-joes-hockey-confirms-pension-age-rises-to-70-for-those-born-after-1965/story-fni6uo1m-1226903894804), but mostly data-ey stories like [this one about speed cameras](http://www.adelaidenow.com.au/news/south-australia/none-of-adelaides-top-10-speed-camera-locations-in-2014-are-on-the-states-black-spot-program/story-fni6uo1m-1226909582332), [this one fact checking the state government's tourism numbers](http://www.adelaidenow.com.au/sport/tourdownunder/tour-down-under-tourist-numbers-in-spotlight-with-abs-statistics-failing-to-support-government-figures/story-fnkqpwk5-1226810893442) or [this unemployment yarn with a tasty embed](http://www.adelaidenow.com.au/news/south-australia/sas-unemployment-rate-for-april-drops-08-per-cent-down-from-12year-high/story-fni6uo1m-1226910164753).
+
+Get in touch with me on [Linkedin](http://www.linkedin.com/in/jacksongs) or on Twitter: [@jacksongs](http://www.twitter.com/jacksongs).
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,9 +3,11 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Jackson Gothe-Snape'
-SITENAME = u'jacksongs'
-SITEURL = ''
+SITENAME = u'jackson.gs'
+SITEURL = 'http://jackson.gs'
 DELETE_OUTPUT_DIRECTORY = False
+
+PROFILE_IMG_URL = 'https://pbs.twimg.com/profile_images/378800000813480994/1487341b80b9053dd39c103a7ca51eef.png'
 
 TIMEZONE = 'Australia/Canberra'
 
@@ -17,27 +19,31 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 THEME = 'themes/pure-single'
-OUTPUT_PATH = 'output'
-PATH = 'content'
-COVER_IMG_URL = 'images/stairs.jpg'
+OUTPUT_PATH = 'output/'
+PATH = None
+PAGE_DIR = 'pages'
+COVER_IMG_URL = 'http://jackson.gs/images/stairs.jpg'
+
+DISPLAY_PAGES_ON_MENU = True
+
+MENUITEMS = (('About', 'pages/about-jackson.html'),)
 
 # Blogroll
-#LINKS =  (('Pelican', 'http://getpelican.com/'),
+#LINKS =  (('About', '/pages/about-jackson.html'),)
 #          ('Python.org', 'http://python.org/'),
 #          ('Jinja2', 'http://jinja.pocoo.org/'),
 #          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-#SOCIAL = (('You can add links in your config file', '#'),
-#          ('Another social link', '#'),)
+SOCIAL =     (('github', 'https://github.com/jacksongs/'),('twitter-square', 'https://twitter.com/jacksongs'),)
+
+TWITTER_USERNAME = 'jacksongs'
+GITHUB_URL = 'https://github.com/jacksongs/'
 
 DEFAULT_PAGINATION = 7
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-GOOGLE_ANALYTICS = 'UA-33090301-1'
-DISQUS_SITENAME = 'jacksongs'
 
 PLUGIN_PATH = 'pelican-plugins'
 PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
